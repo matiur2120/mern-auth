48ad2ab3345bc0618afb71b17d8d792dbd636b8f
matiur <matiur211993@gmail.com>
2024-01-06 00:24:27 +0600

    create header component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -7,6 +8,8 @@ import Signup from "./pages/Signup";
 const App = () => {
   return (
     <Router>
+      {/* header */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
