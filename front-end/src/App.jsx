b3fca1955bba0ee1d9a9f0ffdeba32c014510793
matiur <matiur211993@gmail.com>
2024-01-06 00:03:50 +0600

    add routes and create pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
