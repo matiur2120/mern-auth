015a89f57e82176b6f1b371d3622399542389583
matiur <matiur211993@gmail.com>
2024-01-06 22:31:17 +0600

    create singup api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 dotenv.config()
 const app = express();
+app.use(express.json())
 
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -17,3 +19,4 @@ app.listen(3000, ()=>{
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
