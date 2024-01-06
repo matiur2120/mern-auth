b3dd45d3b272049831f1ea5c954b11e1ea57985c
matiur <matiur211993@gmail.com>
2024-01-06 23:04:56 +0600

    complete sign up page ui

--- front-end/src/pages/Signup.jsx
+++ front-end/src/pages/Signup.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Singup</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-200 p-3 rounded-md"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-200 p-3 rounded-md"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-200 p-3 rounded-md"
+        />
+        <button className="bg-slate-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80">
+          Sign up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500">Sign in</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
