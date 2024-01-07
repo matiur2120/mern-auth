a3e845e7fa375590e4595b81b4dd0edcd8d665c9
matiur <matiur211993@gmail.com>
2024-01-07 21:26:09 +0600

    complete sign in page functionality

--- front-end/src/pages/Signin.jsx
+++ front-end/src/pages/Signin.jsx
@@ -1,5 +1,73 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 const Signin = () => {
-  return <div>Signin</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sing In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-200 p-3 rounded-md"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-200 p-3 rounded-md"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-700">Something went wrong!</p>}
+    </div>
+  );
 };
 
 export default Signin;
